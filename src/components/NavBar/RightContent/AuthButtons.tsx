d6c8eb15420427979db42b0cd89844873cdfce35
stephenm7777 <stephenmartinez7000@gmail.com>
2023-06-26 02:27:03 -0700

    updated corner buttons

--- src/components/NavBar/RightContent/AuthButtons.tsx
+++ src/components/NavBar/RightContent/AuthButtons.tsx
@@ -4,8 +4,22 @@ import {Button} from "@chakra-ui/react"
 export const AuthButton: React.FC = () => {
     return (
         <>
-        <Button>Log In  </Button>
-        <Button> Sign up </Button>
+        <Button variant="outline" 
+                height={'28px'} 
+                display={{base: "none", sm: "flex"}}
+                width={{base: '70px', md: '110px'}}
+                mr={2}
+
+                >Log In
+        </Button>
+
+        <Button height="28px"
+                display={{base: 'none', sm: 'flex'}}
+                width={{base: '70px', md: '110px'}}
+                mr={2}
+                > Sign up 
+        </Button>
+        
         </>
     )
 };
